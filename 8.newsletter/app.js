8fdf1d2ff0d069404af57e3504f7eb9d4a1db970
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-28 00:28:18 -0300

    Removing auth key

---
+++ 8.newsletter/app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const https = require("https");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/singup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.firstName;
+  const secondName = req.body.secondName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: secondName,
+      }
+    }]
+  };
+  
+  const jsonData = JSON.stringify(data);
+  const url = "https://us7.api.mailchimp.com/3.0/lists/231cba2ade";
+  const options = {
+    method: "POST",
+    auth: "lucasb:c5f3f0f97c7e503e361192054208ad68-us7"
+  };
+  const request = https.request(url, options, function(response){
+    if(response.statusCode === 200){
+      res.sendFile(__dirname+"/success.html");
+    }else{
+      res.sendFile(__dirname+"/failure.html");
+    }
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("The server is running on PORT 3000");
+});
+
