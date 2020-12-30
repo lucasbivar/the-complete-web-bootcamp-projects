8d05c266da2516f20993c8c5fe005cd8c03ec3f7
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 16:28:31 -0300

    adding css file

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -8,7 +8,7 @@ let items = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.static("public"));
 app.get('/', function(req, res){
   
   let today = new Date();
