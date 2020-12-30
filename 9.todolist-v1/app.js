1c26e5da5d9fee521cb05e4a1dc5d60fed15951c
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 15:06:08 -0300

    Initializing ejs

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -2,18 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
   
   var today = new Date();
   var currentDay = today.getDay();
+  var day = "";
 
   if(currentDay === 6 || currentDay === 0){
-    res.send("Yay it's the weekend");
+    day = "Weekend";
   }else{
-    res.send("Boo! I have to work");
+    day = "Weekday"
   }
+  res.render("list", {kindOfDay: day});
 });
 
 app.listen(3000, function(){
