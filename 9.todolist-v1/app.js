5a13cbd4ec3ac394c4ed9f2edc98d043e639ab38
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 16:03:29 -0300

    improving todo list item

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { inflateSync } = require('zlib');
 
 const app = express();
+
+var items = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res){
   
   var today = new Date();
-  var currentDay = today.getDay();
   var options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
   var day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItem: items});
 });
 
 app.post('/', function(req, res){
-  const newItem = req.body.newItem;
-  console.log(newItem);
+  items.push(req.body.newItem);
+  res.redirect("/");
 });
 
 
