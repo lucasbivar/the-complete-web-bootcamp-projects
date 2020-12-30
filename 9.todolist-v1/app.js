241502f6f3caf7d8ad71a2be8eb117f822179d1d
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 19:33:45 -0300

    refactoring js codes

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -1,28 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname+"/date.js");
 const { inflateSync } = require('zlib');
 
 const app = express();
 
-let items = [];
-let workItems = [];
+const items = [];
+const workItems = [];
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
 app.get('/', function(req, res){
   
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
+  
   res.render("list", {listTitle: day, newListItem: items});
 });
 
 app.post('/', function(req, res){
-  let item = req.body.newItem;
+  
+  const item = req.body.newItem;
   
   if(req.body.list === "Work"){
     if(item != ""){
@@ -41,8 +40,6 @@ app.post('/', function(req, res){
 app.get('/work', function(req, res){
   res.render("list", {listTitle: "Work List", newListItem: workItems})
 });
-
-
 
 app.listen(3000, function(){
   console.log("Server is running on PORT 3000");
