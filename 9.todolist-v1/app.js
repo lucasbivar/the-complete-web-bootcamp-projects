5a0ab1c13760cfd5787a18f1226e487d03685f53
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 15:29:27 -0300

    refactoring the get day method

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -8,33 +8,12 @@ app.get('/', function(req, res){
   
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Error: current day is equal to: "+ currentDay);
-      break;
-  }
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
   res.render("list", {kindOfDay: day});
 });
 
