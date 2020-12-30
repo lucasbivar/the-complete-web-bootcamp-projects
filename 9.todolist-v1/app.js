dc67de5c8a4c89c877d15454d050241d6a77c008
Lucas Bivar <lucasbivarfonseca@gmail.com>
2020-12-30 15:15:29 -0300

    recognizing the name of week day

--- 9.todolist-v1/app.js
+++ 9.todolist-v1/app.js
@@ -9,11 +9,31 @@ app.get('/', function(req, res){
   var today = new Date();
   var currentDay = today.getDay();
   var day = "";
-
-  if(currentDay === 6 || currentDay === 0){
-    day = "Weekend";
-  }else{
-    day = "Weekday"
+  switch (currentDay) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+      day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+      break;
+    default:
+      console.log("Error: current day is equal to: "+ currentDay);
+      break;
   }
   res.render("list", {kindOfDay: day});
 });
