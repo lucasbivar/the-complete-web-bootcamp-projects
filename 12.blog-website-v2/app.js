8839091ed420bb767bb65abbcbb10657680e6b83
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-04 19:19:44 -0300

    render the correct post by post_id

--- 12.blog-website-v2/app.js
+++ 12.blog-website-v2/app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -25,7 +24,6 @@ const postSchema = mongoose.Schema({
 });
 
 const Post = mongoose.model("Post", postSchema);
-
 
 
 app.get('/', function(req, res){
@@ -59,13 +57,13 @@ app.post('/compose', function(req, res){
   res.redirect('/');
 });
 
-app.get('/posts/:postName', function(req, res){
-  const requestedTitle = _.lowerCase(req.params.postName);
-  for(var i = 0; i < posts.length; i++){
-    if(_.lowerCase(posts[i].title) === requestedTitle){
-      res.render("post.ejs", {postTitle: posts[i].title, postContent: posts[i].content});
+app.get('/posts/:postId', function(req, res){
+  const requestedId = req.params.postId;
+  Post.findOne({_id: requestedId}, function(err, postFound){
+    if(!err){
+      res.render("post.ejs", {postTitle: postFound.title, postContent: postFound.content});
     }
-  }
+  })
 });
 
 app.listen(3000, function() {
