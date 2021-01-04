9e0f8d6bf2368ed791d64a503760cee28b2b7f44
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-04 19:00:55 -0300

    connecting compose page with mongodb

--- 12.blog-website-v2/app.js
+++ 12.blog-website-v2/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 var _ = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -15,6 +16,15 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", { useUnifiedTopology: true, useNewUrlParser: true, });
+
+const postSchema = mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
 
 let posts = [];
 
@@ -35,11 +45,12 @@ app.get('/compose', function(req, res){
 });
 
 app.post('/compose', function(req, res){
-  const post = {
+  
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody,
-  };
-  posts.push(post);
+  });
+  post.save();
 
   res.redirect('/');
 });
