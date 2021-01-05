15c05efda217ed914c33f776722d260de257952c
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-05 15:05:07 -0300

    adding delete all route

--- 13.wiki-api/app.js
+++ 13.wiki-api/app.js
@@ -43,6 +43,16 @@ app.post("/articles", function(req, res){
   });
 });
 
+app.delete("/articles", function(req, res){
+  Article.deleteMany( function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    }else{
+       res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
