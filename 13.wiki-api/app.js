4082529c9b748d380e90340761e0cffd9a6f277e
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-05 13:47:37 -0300

    starting mongoose

--- 13.wiki-api/app.js
+++ 13.wiki-api/app.js
@@ -9,6 +9,14 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+
+const articleSchema = mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = mongoose.model("Article", articleSchema);
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
