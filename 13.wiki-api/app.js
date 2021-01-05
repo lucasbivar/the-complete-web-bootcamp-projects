3ed49f0b32fb1cbcf988103197b80434dcc89658
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-05 14:02:43 -0300

    refactoring files and adding get all route

--- 13.wiki-api/app.js
+++ 13.wiki-api/app.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { static } = require("express");
 
 const app = express();
-app.set("view engine", "ejs");
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const articleSchema = mongoose.Schema({
   title: String,
@@ -17,6 +16,16 @@ const articleSchema = mongoose.Schema({
 });
 
 const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function(req, res){
+  Article.find({}, function(err, foundArticles){
+    if(!err){
+      res.send(foundArticles);
+    }else{
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
