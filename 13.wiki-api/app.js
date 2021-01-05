4255f04a0e6a432eac015b087f5a18d76d2d9c1e
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-05 14:15:50 -0300

    add post rout

--- 13.wiki-api/app.js
+++ 13.wiki-api/app.js
@@ -27,6 +27,22 @@ app.get("/articles", function(req, res){
   });
 });
 
+app.post("/articles", function(req, res){
+  
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article.")
+    }else{
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
