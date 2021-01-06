fad8decb00cdfee15feaa46741e31b36b61882f8
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-06 14:14:08 -0300

    implemented register/login feature

--- 14.secrets/app.js
+++ 14.secrets/app.js
@@ -3,11 +3,22 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model("User", userSchema);
+
 
 app.get("/", function(req, res) {
   res.render("home");
@@ -17,10 +28,45 @@ app.get("/login", function(req, res) {
   res.render("login");
 });
 
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne(
+    {email:username ,}
+    ,function (err, foundUser) {
+      if(!err){
+        if(foundUser){
+          if(foundUser.password === password){
+            res.render("secrets");
+          }
+        }
+      }else{
+        console.log(err);
+      }
+    }
+  );
+})
+
 app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.post("/register", function(req, res) {
+
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save(function(err) {
+    if(err){
+      console.log(err);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
 
 app.listen(3000, function() {
   console.log("Server is running on PORT 3000");
