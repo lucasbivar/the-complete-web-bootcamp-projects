5b074611a3bd8c19688f83d987cfb6cb2251c7a9
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-06 14:46:11 -0300

    database encryption

--- 14.secrets/app.js
+++ 14.secrets/app.js
@@ -1,9 +1,12 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
 
 const app = express();
 app.use(express.static("public"));
@@ -12,10 +15,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
