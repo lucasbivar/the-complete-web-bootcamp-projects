34dc9fcf994ec35af0f47996d8ae4047afd6ef1a
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-17 18:04:52 -0300

    create header component

--- 15.keeper-app/src/components/App.jsx
+++ 15.keeper-app/src/components/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import Header from "./Header";
 
 export default function App(){
 
-  return (<h1>Hello World</h1>);
-
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
---
+++ 15.keeper-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header(){
+  return(
+    <header>
+      <h1>Keeper</h1>
+    </header>
+  );
+}
