aef06b5c80338ec5bfbf98af205cce006ea9aa43
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-17 18:33:10 -0300

    create note component

--- 15.keeper-app/src/components/App.jsx
+++ 15.keeper-app/src/components/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
 
 export default function App(){
-
   return (
     <div>
       <Header />
+      <Note />
       <Footer />
     </div>
   );
