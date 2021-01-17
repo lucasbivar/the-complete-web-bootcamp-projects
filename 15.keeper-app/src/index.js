06e520503a27a433bc364a055f20901ee8e89dd5
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-17 17:59:56 -0300

    create app component

--- 15.keeper-app/src/index.js
+++ 15.keeper-app/src/index.js
@@ -10,3 +10,11 @@
 //https://l1pp6.csb.app/
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);
