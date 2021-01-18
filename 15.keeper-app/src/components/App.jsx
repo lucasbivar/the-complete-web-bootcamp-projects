ada1c00b5c12f4debbd22fdc028d9df31a05f2b9
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-18 13:27:10 -0300

    mapping data to note component and changing style

--- 15.keeper-app/src/components/App.jsx
+++ 15.keeper-app/src/components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 export default function App(){
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(note => (
+        <Note 
+          key={note.key} 
+          title={note.title} 
+          content={note.content} />
+        ))}
       <Footer />
     </div>
   );
--- 15.keeper-app/src/components/Note.jsx
+++ 15.keeper-app/src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default function Note(){
+export default function Note(props){
   return(
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
     </div>
   );
 }
