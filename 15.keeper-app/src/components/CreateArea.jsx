ae89a0ae1803c2e89b71dca85c325edf677a223a
Lucas Bivar <lucasbivarfonseca@gmail.com>
2021-01-19 20:45:40 -0300

    keeps track of the title and content

--- 15.keeper-app/src/components/CreateArea.jsx
+++ 15.keeper-app/src/components/CreateArea.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function CreateArea() {
+
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event){
+    const {name, value} = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]:value
+      }
+    });
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+        <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+        <textarea name="content" onChange={handleChange} value={note.content} placeholder="Take a note..." rows="3" />
         <button>Add</button>
       </form>
     </div>
